d0410d6ac4df41a1e1b0a9fb289a21352758eeb1
farrux <kfarrux2005@gmail.com>
2023-03-29 02:42:25 +0500

    dev

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -90,7 +90,7 @@ const getByIdStudent = async(req,res)=>{
 
 const deleteByIdStudent = async(req,res)=>{
     try {
-        await deleteStudent(req,res);
+        await studentDelete(req,res);
     } catch (error) {
         res.status(500).json({
             error: error.message
