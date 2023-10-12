c84fb6d34fbfd61f1835d673f20ab077f6f40340
farrux <kfarrux2005@gmail.com>
2023-10-12 13:01:31 +0500

    Delete Withdraw

--- src/routers/withdraw.router.js
+++ src/routers/withdraw.router.js
@@ -22,7 +22,7 @@ router.post('/', async(req,res)=> {
     }
 })
 
-router.get('/:id', async(req,res)=>{
+router.delete('/:id', async(req,res)=>{
     try {
         await withdrawDeleteController(req,res);
     } catch (error) {
