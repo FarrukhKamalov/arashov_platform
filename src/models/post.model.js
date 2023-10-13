7af24cc58cf5ae0a5bb390d3f528f81a63fd1558
farrux <kfarrux2005@gmail.com>
2023-10-14 02:33:54 +0500

    Get Time

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -17,7 +17,7 @@ const PostSchema = new mongoose.Schema({
     ],
     createdAt: {
         type: Date,
-        default: new Date().toLocaleString()
+        default: new Date().toLocaleString('uz-UZ')
     }
 });
 
