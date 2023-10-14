b9044f3b4808c82f83adad83e36d184ea1946035
farrux <kfarrux2005@gmail.com>
2023-10-14 19:18:17 +0500

    OTP 5 number

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -14,7 +14,7 @@ const referralCodeGenerator = async () => {
 
 const sendOTPEmail = async (email) => {
     try {
-        const otp = Math.floor(Math.random() * 100000);
+        const otp = Math.floor(10000 + Math.random() * 100000);
 
         const transporter = nodemailer.createTransport({
             // host: "smtp.ethereal.email",
