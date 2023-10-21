bca5b1be40ec9b9d89ba796880b763dd4976f124
farrux <kfarrux2005@gmail.com>
2023-10-21 23:00:54 +0500

    Apple icon Height

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -45,7 +45,7 @@ const sendOTPEmail = async (email) => {
       <hr style="width: 80%; opacity: 0.2;">
       <div style="display: flex; justify-content: space-around; align-items: center; ">
           <a href="">
-              <img src="https://seeklogo.com/images/D/download-on-the-app-store-flat-badge-logo-4582694404-seeklogo.com.png" width="100px">
+              <img src="https://seeklogo.com/images/D/download-on-the-app-store-flat-badge-logo-4582694404-seeklogo.com.png" width="100px" height="34px">
           </a>
         <a href="">
           <img src="https://seeklogo.com/images/G/get-it-on-google-play-badge-logo-8CDE582776-seeklogo.com.png" width="100px" height="34px">
